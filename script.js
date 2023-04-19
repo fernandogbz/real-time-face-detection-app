2c3d5053f50ff78999c9e4ea02434e857ce67de9
Fernando Jimenez <fgjv06@gmail.com>
2023-04-19 19:16:13 -0400

    JS - stream method takes whats coming from the webcam, and with video.srcObjet I set it as the source of the video

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const video = document.getElementById("video");
 function startVideo() {
   navigator.mediaDevices.getUserMedia(
     {video: {} },
+    stream => video.srcObject = stream,
   )
-}
+};
