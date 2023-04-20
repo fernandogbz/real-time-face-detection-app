5480ca10029949c2133789834fce082f036368e5
Fernando Jimenez <fgjv06@gmail.com>
2023-04-20 18:58:06 -0400

    JS - promise to load all the models

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//Getting video variable
 const video = document.getElementById("video");
 
+//Hooking up the webcam to the video element
 function startVideo() {
+  // navigator.getUserMedia takes a parameter, so video is the key, with an empty object as the parameter
   navigator.getUserMedia(
     {video: {} },
+    // stream method takes whats coming from the webcam, and with video.srcObjet I set it as the source of the video
     stream => video.srcObject = stream,
+    // if we get an error just log that in the console
     error => console.error(error)
   )
 };
