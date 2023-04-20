3855cd7f145c0cf73f0d103554ef032d727804aa
Fernando Jimenez <fgjv06@gmail.com>
2023-04-20 19:27:13 -0400

    JS - This is done asynchronously, so 'Promise.all' is going to run all these calls in parallel, to make it much quicker to execute

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //Getting video variable
 const video = document.getElementById("video");
 
+//Load the different models
+// This is done asynchronously, so 'Promise.all' is going to run all these calls in parallel, to make it much quicker to execute
+Promise.all([
+  
+  
+])
+
 //Hooking up the webcam to the video element
 function startVideo() {
   // navigator.getUserMedia takes a parameter, so video is the key, with an empty object as the parameter
@@ -8,7 +15,7 @@ function startVideo() {
     {video: {} },
     // stream method takes whats coming from the webcam, and with video.srcObjet I set it as the source of the video
     stream => video.srcObject = stream,
-    // if we get an error just log that in the console
+    // if there's an error just log that in the console
     error => console.error(error)
   )
 };
