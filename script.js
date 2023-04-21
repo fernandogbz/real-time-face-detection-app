d74928acccb2f4fdbe557ad11059579271920953
Fernando Jimenez <fgjv06@gmail.com>
2023-04-20 20:32:37 -0400

    JS - Add that canvas to the screen

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ function startVideo() {
 video.addEventListener('play', () => {
   //Create canvas from the video element
   const canvas = faceapi.createCanvasFromMedia(video);
+  //Add that canvas to the screen
+  document.body.append(canvas);
   // Set interval cause the code is gonna run multiple times in a row, asynchronous cause is an asynchronous library
   setInterval(async () => {
     // Detections is going to await the faceapi, it's going to get all the faces inside of the webcam image, we're going to do this every 100 miliseconds
