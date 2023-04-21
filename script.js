68c8119a0720cbd2625e1778acf02fe461448bf8
Fernando Jimenez <fgjv06@gmail.com>
2023-04-20 20:43:12 -0400

    JS - Draw the detections in the canvas

--- script.js
+++ script.js
@@ -38,6 +38,8 @@ video.addEventListener('play', () => {
     console.log(detections);
     //ResizedDetections is gonna make it so that the boxes that show up around the face are properly sized for the video element that I'm using as well as for the canvas
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    //Draw the detections in the canvas
+    faceapi.draw.drawDetections(canvas, resizedDetections);
   }, 100)
 });
 
